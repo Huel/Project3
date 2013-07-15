aa1689b754e53201ecc6dd1cf1f3c5222cd502d8
Sadarian <tobfrey@web.de>
2013-07-15 14:24:08 +0200

    Network + teamTis

---
+++ Assets/Scripts/LocalPlayerControler.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class LocalPlayerControler : MonoBehaviour
+{
+	public int teamID = -1;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void PlayerRespawn() 
+	{
+
+	}
+}
---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour 
+{
+	public const string localPlayerControler = "LocalPlayerControler";
+	public const string networkManager = "NetworkManager";
+}
