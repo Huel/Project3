7daae370809a2ca6ca8441ccfe6e260429f4f6a1
Sadarian <tobfrey@web.de>
2013-07-16 12:56:35 +0200

    Network + teamTis (reverse-merged from commit aa1689b754e53201ecc6dd1cf1f3c5222cd502d8)

--- Assets/Scripts/Tags.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class Tags : MonoBehaviour 
-{
-	public const string localPlayerControler = "LocalPlayerControler";
-	public const string networkManager = "NetworkManager";
-}
