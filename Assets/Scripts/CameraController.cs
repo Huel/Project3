ea0d8269a242eb6d0861d9a46aef1b3b075b7ec2
Salzmann <kirill.salzmann@web.de>
2013-07-17 14:59:33 +0200

    asd

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
+    public float speed = 1;
+    public float zeldaSpeed = 1;
     public float rotationSpeed = 4;
     private Quaternion xCorrection;
     private float lastRotationCheck;
-    public float rotationCheck = 0.0f;
+    private float rotationCheck = 0.0f;
     public int obereKameraGrenze = 50;
     public int untereKameraGrenze = -50;
-    public bool frozenYDown = false;
-    public bool frozenYUp = false;
+    private bool frozenYDown = false;
+    private bool frozenYUp = false;
+    public int selectedCameraBehaviour = 0;
     // Use this for initialization
     void Start()
     {
@@ -20,6 +22,50 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (selectedCameraBehaviour)
+        {
+            case 0:
+                DefaultCameraLogic();
+                break;
+            case 1:
+                ZeldaCameraLogic();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void FreezeYDown(bool value)
+    {
+        frozenYDown = value;
+    }
+
+    public void FreezeYUp(bool value)
+    {
+        frozenYUp = value;
+    }
+
+    private void ZeldaCameraLogic()
+    {
+
+    }
+
+    private void DefaultCameraLogic()
+    {
+        //camera direction
+        Vector3 forward = transform.FindChild("cameraPivot").FindChild("cameraCorrection").FindChild("Camera").TransformDirection(Vector3.forward);
+        //doesn't need y
+        forward.y = 0;
+        forward.Normalize();
+        Vector3 right = new Vector3(forward.z, 0, -forward.x);
+
+        float v = Input.GetAxisRaw("leftanalogY");
+
+        Vector3 targetDirection = v * forward;
+        targetDirection = targetDirection.normalized * speed;
+        targetDirection.y = -1;
+        GetComponent<CharacterController>().Move(targetDirection);
+
         transform.Rotate(0, Input.GetAxis("rightanalogX") * rotationSpeed, 0);
         transform.Rotate(0, Input.GetAxis("leftanalogX") * rotationSpeed, 0);
         if ((Input.GetAxis("rightanalogY") <= 0f && !frozenYDown) || (Input.GetAxis("rightanalogY") >= 0f && !frozenYUp) || transform.FindChild("cameraPivot").localEulerAngles.x < untereKameraGrenze || transform.FindChild("cameraPivot").localEulerAngles.x > obereKameraGrenze)
@@ -41,15 +87,5 @@ public class CameraController : MonoBehaviour
             }
             lastRotationCheck = rotationCheck;
         }
-    }
-
-    public void FreezeYDown(bool value)
-    {
-        frozenYDown = value;
-    }
-
-    public void FreezeYUp(bool value)
-    {
-        frozenYUp = value;
     }
 }
---
+++ Assets/Scripts/CameraControllerZelda.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraControllerZelda : MonoBehaviour 
+{
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	
+	}
+}
