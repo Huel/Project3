174dd362094f73a2ec72fee61ec7f560b1bf7ea4
Robert Lehmann <Deathwing_Shadow@hotmail.de>
2013-07-18 10:30:12 +0200

    -XMLReader update

---
+++ Assets/Scripts/XML.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class XML : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        XMLReader test = new XMLReader(Path.GetFullPath("Assets/Scripts/XML/test.xml"));
+        Debug.Log(test.text());
+        Debug.Log(Path.GetFullPath("Assets/Scripts/XML/test.xml"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/XMLReader.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.IO;
+
+public class XMLReader
+{
+    private ArrayList _skillArray;
+    private ArrayList _skillParameters;
+    private XmlDocument xmlDoc;
+
+    public XMLReader(string path)
+    {
+        xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(File.ReadAllText(path));
+
+        XmlNodeList skills = xmlDoc.GetElementsByTagName("skill");//skills in an array
+
+        //foreach (XmlNode skill in skills)
+        //{
+         //   XmlNodeList skillComponent = skill.ChildNodes;//all components of the current skill
+         //   _skillParameters = new ArrayList();
+         //   foreach (XmlNode skillParameters in skillComponent)//each component has two relevant values we want: Name and InnerText
+         //   {
+         //       _skillParameters.Add(skillParameters.Name);
+        //        _skillParameters.Add(skillParameters.InnerText);
+        //    }
+        //    _skillArray.Add(_skillParameters);//each _skillParameters is one entire skill
+        //}
+
+
+        //foreach(ArrayList a in _skillArray)
+        //{
+        //    for (int i = 0; i < a.Count; i+=2 )
+        //    {
+        //        Debug.Log(a[i]+": "+a[i+1]);
+        //    }
+        //}
+    }
+
+    public string text()
+    {
+        return xmlDoc.InnerText;
+    }
+
+    public ArrayList skillArray //getter
+    {
+        get
+        {
+            return _skillArray;
+        }
+    }
+}
