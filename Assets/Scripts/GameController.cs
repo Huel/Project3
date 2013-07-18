87e320b0bd4bd66a7914605b599cb4395727a783
Sadarian <tobfrey@web.de>
2013-07-18 10:29:35 +0200

    Network:     -Creating Server     - Join server     - switch scene

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public int teamID = -1;
+	// Use this for initialization
+	void Awake()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
