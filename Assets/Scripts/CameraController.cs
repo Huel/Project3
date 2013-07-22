ce7ce4fb90df366d15f21d90105b2fbb7554943a
Salzmann <kirill.salzmann@web.de>
2013-07-22 14:29:17 +0200

    Camera Rotation uses SLerp to change now

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,20 +2,27 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private bool moveTowardsZelda = false;
     [SerializeField]
     private float _zeldaDistanceAway = 10.0f;
     [SerializeField]
     private float _zeldaDistanceUp = 5.0f;
     [SerializeField]
-    private float _cameraLerpSpeed = 2.0f;
+    private float _cameraMovementSpeed = 2.0f;
+    [SerializeField]
+    private float _cameraRotationSpeed = 100.0f;
     [SerializeField]
     private Transform _player;
     [SerializeField]
     private Transform _cameraDestination;
 
+
+    private GameObject limitRotation;
+    private bool moveTowardsZelda = false;
+
+
     void Start()
     {
+        limitRotation = new GameObject();
         _cameraDestination.position = _player.position + Vector3.up * _zeldaDistanceUp - _player.FindChild("Player").forward * _zeldaDistanceAway;
     }
 
@@ -32,22 +39,18 @@ public class CameraController : MonoBehaviour
 
         if (!moveTowardsZelda)
         {
-            if (Value((_player.position - transform.position).magnitude) > _zeldaDistanceAway)
-                transform.position = Vector3.Lerp(transform.position, _player.position + Vector3.up * _zeldaDistanceUp - _player.FindChild("Player").forward * _zeldaDistanceAway, Time.deltaTime * _cameraLerpSpeed);
+            if (Mathf.Abs((_player.position - transform.position).magnitude) > _zeldaDistanceAway)
+                transform.position = Vector3.Lerp(transform.position, _player.position + Vector3.up * _zeldaDistanceUp - _player.FindChild("Player").forward * _zeldaDistanceAway, Time.deltaTime * _cameraMovementSpeed);
         }
         else
         {
-            transform.position = Vector3.Lerp(transform.position, _cameraDestination.position, Time.deltaTime * _cameraLerpSpeed);
+            transform.position = Vector3.Lerp(transform.position, _cameraDestination.position, Time.deltaTime * _cameraMovementSpeed);
         }
 
-        transform.LookAt(_player);
-    }
+        limitRotation.transform.position = transform.position;
+        limitRotation.transform.rotation = transform.rotation;
+        limitRotation.transform.LookAt(_player);
 
-
-    float Value(float Input)
-    {
-        if (Input < 0)
-            return -Input;
-        return Input;
+        transform.rotation = Quaternion.Slerp(transform.rotation, limitRotation.transform.rotation, Time.deltaTime * _cameraRotationSpeed);
     }
 }
