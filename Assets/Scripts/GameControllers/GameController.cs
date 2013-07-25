8cf464652193742c18f2f164e912dc5958536980
Sadarian <tobfrey@web.de>
2013-07-25 16:42:16 +0200

    Quit Game with message

--- Assets/Scripts/GameControllers/GameController.cs
+++ Assets/Scripts/GameControllers/GameController.cs
@@ -1,44 +1,85 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public enum GameState { Starting, Running, Ended };
+	public enum GameState { Starting, Running, Ended };
 
-    private float _startTime;
-    public GameState state;
-    private List<NetworkPlayerController> _networkPlayerControllers = new List<NetworkPlayerController>();
+	private float _startTime;
+	public GameState state;
+	private bool gameEndet;
+	private float passedTime = 0f;
+	private List<NetworkPlayerController> _networkPlayerControllers = new List<NetworkPlayerController>();
 
-    public NetworkPlayerController GetNetworkPlayerController(NetworkPlayer player)
-    {
-        foreach (NetworkPlayerController controller in _networkPlayerControllers)
-        {
-            if (controller.networkPlayer == player)
-            {
-                return controller;
-            }
-        }
-        return null;
-    }
+	public NetworkPlayerController GetNetworkPlayerController(NetworkPlayer player)
+	{
+		foreach (NetworkPlayerController controller in _networkPlayerControllers)
+		{
+			if (controller.networkPlayer == player)
+			{
+				return controller;
+			}
+		}
+		return null;
+	}
 
-    public float GameTime
-    {
-        get { return Time.time - _startTime; }
-    }
+	public float GameTime
+	{
+		get { return Time.time - _startTime; }
+	}
 
-    [RPC]
-    void AddNetworkPlayerController(int playerID, string playerName, int Team, NetworkPlayer networkPlayer)
-    {
-        _networkPlayerControllers.Add(new NetworkPlayerController(playerID, playerName, Team, networkPlayer));
-    }
+	void OnPlayerDisconnected()
+	{
+		networkView.RPC("SetGameState", RPCMode.AllBuffered, (int)GameState.Ended);
+	}
 
-    [RPC]
-    void SetGameState(int state)
-    {
-        this.state = (GameState)state;
-        if (this.state == GameState.Running)
-        {
-            _startTime = Time.time;
-        }
-    }
+	void OnDisconnectedFromServer()
+	{
+		networkView.RPC("SetGameState", RPCMode.AllBuffered, (int)GameState.Ended);
+	}
+
+	void OnGUI()
+	{
+		if (gameEndet)
+		{
+			GUI.TextArea(new Rect(Screen.width * 0.5f, Screen.height * 0.5f, Screen.width * 0.2f, Screen.height * 0.1f),
+			             "A player has been disconnected from the Game");
+			Debug.Log("Has Disconnected");
+		}
+	}
+
+	void Update()
+	{
+		if (gameEndet)
+		{
+			passedTime += Time.deltaTime;
+
+			if (passedTime >= 3)
+			{
+				Application.Quit();
+			}
+		}
+	}
+
+	[RPC]
+	void AddNetworkPlayerController(int playerID, string playerName, int Team, NetworkPlayer networkPlayer)
+	{
+		_networkPlayerControllers.Add(new NetworkPlayerController(playerID, playerName, Team, networkPlayer));
+	}
+
+	[RPC]
+	void SetGameState(int state)
+	{
+		this.state = (GameState)state;
+		if (this.state == GameState.Running)
+		{
+			_startTime = Time.time;
+		}
+
+		if (this.state == GameState.Ended)
+		{
+			gameEndet = true;
+		}
+	}
 }
