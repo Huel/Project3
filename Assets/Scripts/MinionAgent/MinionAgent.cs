2000e09c3f94dcef942d56772e03f75b1c8cea8b
Maximilian Frank <Maximilian.F1@gmx.de>
2013-07-26 12:06:08 +0200

    Adding MinionAgent changes

--- Assets/Scripts/MinionAgent/MinionAgent.cs
+++ Assets/Scripts/MinionAgent/MinionAgent.cs
@@ -1,25 +1,19 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class MinionAgent : MonoBehaviour
 {
-    public enum LaneIdentifier { Lane1, Lane2, Lane3 };
+    public enum LaneIdentifier{ Lane1, Lane2, Lane3 };
     public LaneIdentifier laneID;
 
-    private NavMeshAgent agent;
-    private Skill basicAttack;
+    private NavMeshAgent _agent;
+    private Skill _basicAttack;
 
-<<<<<<< HEAD
-    public Target _destination;    // default target
-    public Target _origin;         // came from here
-    public Target _target;         // current target
-    private float destinationOffset = 1f;
-=======
     private Target _destination;    // default target
     private Target _origin;         // came from here
     private Target _target;         // current target
     private float _destinationOffset = 1f;
->>>>>>> refs/heads/feature/optimize_collider'n'stuff
 
     public Range attentionRange;
     public Range looseAttentionRange;
@@ -29,21 +23,6 @@ public class MinionAgent : MonoBehaviour
 
     void Start()
     {
-<<<<<<< HEAD
-        agent = gameObject.GetComponent<NavMeshAgent>();
-        basicAttack = gameObject.AddComponent<Skill>();
-        basicAttack.Init("Basic Attack");
-
-        if (networkView.isMine)
-            agent.enabled = true;
-    }
-
-    void Update()
-    {
-        if (!networkView.isMine)
-            this.enabled = false;
-        agent.speed = gameObject.GetComponent<Speed>().CurrentSpeed;
-=======
         _agent = gameObject.GetComponent<NavMeshAgent>();
         _basicAttack = gameObject.AddComponent<Skill>();
         _basicAttack.Init("Basic Attack");
@@ -80,19 +59,13 @@ public class MinionAgent : MonoBehaviour
         _agent.speed = gameObject.GetComponent<Speed>().CurrentSpeed;
         
         // no target
->>>>>>> refs/heads/feature/optimize_collider'n'stuff
         if (_target == null)
         {
-            agent.enabled = true;
+            _agent.enabled = true;
             if (_destination != null)
             {
-<<<<<<< HEAD
-                agent.destination = _destination.Position;
-                if (_destination.GetDistance(transform.position) <= destinationOffset)
-=======
                 _agent.destination = _destination.Position;
                 if (_destination.GetDistance(transform.position) <= _destinationOffset)
->>>>>>> refs/heads/feature/optimize_collider'n'stuff
                 {
                     CheckPoint checkPoint = _destination.gameObject.GetComponent<CheckPoint>();
                     if (checkPoint != null)
@@ -100,24 +73,6 @@ public class MinionAgent : MonoBehaviour
                         Target newDestination = checkPoint.GetNextCheckPoint(_origin);
                         SetOrigin(_destination);
                         SetDestination(newDestination);
-<<<<<<< HEAD
-
-                    }
-                    else
-                    {
-                        SetDestination(null);
-                    }
-                }
-
-            }
-            else
-            {
-                //if it hasn't a destination reset to its position
-                agent.destination = transform.position;
-            }
-            if (attentionRange != null)
-                SelectTarget();
-=======
                     }
                     else
                         SetDestination(null);
@@ -128,35 +83,35 @@ public class MinionAgent : MonoBehaviour
                 _agent.destination = transform.position;
             if (attentionRange != null && attentionRange.gameObject.active)
                 SelectTarget();       
->>>>>>> refs/heads/feature/optimize_collider'n'stuff
         }
-        else if (agent.enabled)
-            agent.destination = _target.gameObject.transform.position;
-        if (looseAttentionRange != null && _target != null)
-            if (!looseAttentionRange.isInRange(_target))
+        // already has target
+        else
+        {
+            //if target out of range
+            if (looseAttentionRange != null && looseAttentionRange.gameObject.active 
+                && !looseAttentionRange.isInRange(_target))
             {
                 _target = null;
-
+                return;
             }
-        if (_target != null)
-            basicAttack.Execute();
+            if (_agent.enabled)
+                _agent.destination = _target.gameObject.transform.position;
+            if (gameObject.GetComponent<Team>().ID != _target.gameObject.GetComponent<Team>().ID)
+                _basicAttack.Execute();  // --> start fight ??
+        }
+        
+            
     }
 
     void SelectTarget()
     {
-        Team team;
         var target = attentionRange.GetNearestTargetByTypePriorityAndEnemyOnly(new List<TargetType> { TargetType.Minion, TargetType.Hero, TargetType.Valve, TargetType.Checkpoint }, GetComponent<Team>());
         if (target == null) return;
-
         if (target.type == TargetType.Minion || target.type == TargetType.Hero)
         {
-            team = target.gameObject.GetComponent<Team>();
-            if (team != null && team.isEnemy(GetComponent<Team>()))
-            {
-                _target = target;
-                contact.AddListener(target, OnEnemyContact);
-                return;
-            }
+            _target = target;
+            contact.AddListener(target, OnEnemyContact);
+            return;
         }
 
         if (target.type == TargetType.Valve)
@@ -168,7 +123,7 @@ public class MinionAgent : MonoBehaviour
 
     private void OnEnemyContact(Target target)
     {
-        //agent.enabled = false;
+        //_agent.enabled = false;
         //basicSkill.Execute();
     }
 
