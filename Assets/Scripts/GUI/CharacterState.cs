23f6f5c5c9343a4c2d85fd030148e9c2df193e08
Anton Licht <anton.licht@gmx.de>
2013-08-02 13:16:23 +0200

    Trophy UI

--- Assets/Scripts/GUI/CharacterState.cs
+++ Assets/Scripts/GUI/CharacterState.cs
@@ -8,12 +8,19 @@ public class CharacterState : MonoBehaviour
     private UISprite _healthBar;
     private Speed _speedComp;
     private UISprite _staminaBar;
+    private Trophy _trophyComp;
+    private GameObject _trophy01;
+    private GameObject _trophy02;
+    private GameObject _trophy03;
 
     // Use this for initialization
     void Awake()
     {
         _healthBar = transform.FindChild("health").GetComponent<UISprite>();
         _staminaBar = transform.FindChild("stamina").GetComponent<UISprite>();
+        _trophy01 = transform.FindChild("trophy01").gameObject;
+        _trophy02 = transform.FindChild("trophy02").gameObject;
+        _trophy03 = transform.FindChild("trophy03").gameObject;
     }
 
     // Update is called once per frame
@@ -21,18 +28,7 @@ public class CharacterState : MonoBehaviour
     {
         UpdateHealth();
         UpdateStamina();
-    }
-
-    private void UpdateStamina()
-    {
-        if (!_speedComp)
-        {
-            if (FindPlayer())
-                _speedComp = _player.GetComponent<Speed>();
-            else
-                return;
-        }
-        _staminaBar.fillAmount = _speedComp.Stamina / _speedComp.MaxStamina;
+        UpdateTrophies();
     }
 
     private bool FindPlayer()
@@ -65,5 +61,51 @@ public class CharacterState : MonoBehaviour
         }
         _healthBar.fillAmount = _healthComp.HealthPoints / _healthComp.MaxHealth;
 
+    }
+
+    private void UpdateStamina()
+    {
+        if (!_speedComp)
+        {
+            if (FindPlayer())
+                _speedComp = _player.GetComponent<Speed>();
+            else
+                return;
+        }
+        _staminaBar.fillAmount = _speedComp.Stamina / _speedComp.MaxStamina;
+    }
+
+    private void UpdateTrophies()
+    {
+        if (!_trophyComp)
+        {
+            if (FindPlayer())
+                _trophyComp = _player.GetComponent<Trophy>();
+            else
+                return;
+        }
+        switch (_trophyComp.trophyLevel)
+        {
+            case 3:
+                _trophy03.SetActive(true);
+                _trophy02.SetActive(true);
+                _trophy01.SetActive(true);
+                break;
+            case 2:
+                _trophy03.SetActive(false);
+                _trophy02.SetActive(true);
+                _trophy01.SetActive(true);
+                break;
+            case 1:
+                _trophy03.SetActive(false);
+                _trophy02.SetActive(false);
+                _trophy01.SetActive(true);
+                break;
+            default:
+                _trophy03.SetActive(false);
+                _trophy02.SetActive(false);
+                _trophy01.SetActive(false);
+                break;
+        }
     }
 }
