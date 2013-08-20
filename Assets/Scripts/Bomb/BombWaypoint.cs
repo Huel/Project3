ba5c37cf1323645881262467028ac2b072717676
Salzmann <kirill.salzmann@web.de>
2013-08-20 12:16:25 +0200

    updates

--- Assets/Scripts/Bomb/BombWaypoint.cs
+++ Assets/Scripts/Bomb/BombWaypoint.cs
@@ -4,4 +4,14 @@ public class BombWaypoint : MonoBehaviour
 {
     public GameObject WaypointA;
     public GameObject WaypointB;
+    public GameObject BombStopper;
+
+    public bool GetAllowPassage()
+    {
+        if (BombStopper != null
+            && BombStopper.GetComponent<Valve>() != null
+            && BombStopper.GetComponent<Valve>().GetValveState() != Valve.ValveState.Closed)
+            return false;
+        return true;
+    }
 }
