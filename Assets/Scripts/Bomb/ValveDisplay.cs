4c0dfc5d2a65d8d4b07642c9e40b0822c5273bb3
Maximilian Frank <Maximilian.F1@gmx.de>
2013-08-21 11:36:00 +0200

    new Minion new Minion Textures new ShortCut, New Shrooms, New pipes with Textures and much more

---
+++ Assets/Scripts/Bomb/ValveDisplay.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class ValveDisplay : MonoBehaviour
+{
+    public GameObject display;
+    public GameObject valve;
+    
+    private float minRotation = -90f;
+    private float maxRotation = 90f;
+
+    public float currentRotation;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
--- Assets/Scripts/Minion/MinionVisualsController.cs
+++ Assets/Scripts/Minion/MinionVisualsController.cs
@@ -6,6 +6,7 @@ public class MinionVisualsController : MonoBehaviour
     private string run = "Run";
     private string dead = "Dead";
     private string attack = "Attack";
+    private string attackType = "AttackType";
     private bool[] checkChange = new bool[3];
 
     void Start()
@@ -26,6 +27,9 @@ public class MinionVisualsController : MonoBehaviour
 
         if (animator.GetBool(run) && GetComponent<Skill>().State == SkillState.InExecution)
         {
+            int attackValue = (Random.Range(1, 3));
+            animator.SetInteger(attackType, attackValue);
+
             animator.SetBool(run, false);
             animator.SetBool(attack, true);
         }
@@ -44,7 +48,7 @@ public class MinionVisualsController : MonoBehaviour
         }
         if (checkChange[0] != animator.GetBool(run) || checkChange[1] != animator.GetBool(attack) || checkChange[2] != animator.GetBool(dead))
         {
-            networkView.RPC("TransferAnimStates", RPCMode.OthersBuffered, animator.GetBool(run), animator.GetBool(attack), animator.GetBool(dead));
+            networkView.RPC("TransferAnimStates", RPCMode.OthersBuffered, animator.GetBool(run), animator.GetBool(attack), animator.GetBool(dead), animator.GetInteger(attackType));
             checkChange[0] = animator.GetBool(run);
             checkChange[1] = animator.GetBool(attack);
             checkChange[2] = animator.GetBool(dead);
@@ -52,10 +56,11 @@ public class MinionVisualsController : MonoBehaviour
     }
 
     [RPC]
-    public void TransferAnimStates(bool first, bool second, bool third)
+    public void TransferAnimStates(bool first, bool second, bool third, int type)
     {
         animator.SetBool(run, first);
         animator.SetBool(attack, second);
         animator.SetBool(dead, third);
+        animator.SetInteger(attackType, type);
     }
 }
