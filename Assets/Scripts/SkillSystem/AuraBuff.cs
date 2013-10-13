423b15802ce0dccbd8b03c5896e5cb033922515c
Maximilian Frank <Maximilian.F1@gmx.de>
2013-10-13 13:20:03 +0200

    AuraBuff created

---
+++ Assets/Scripts/SkillSystem/AuraBuff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class AuraBuff : MonoBehaviour
+{
+    private List<Target> targets; 
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
