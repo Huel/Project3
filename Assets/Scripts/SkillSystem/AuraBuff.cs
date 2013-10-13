80d60137950477f6a3574cc84e836891eb2c0a60
Maximilian Frank <Maximilian.F1@gmx.de>
2013-10-13 13:42:15 +0200

    .

--- Assets/Scripts/SkillSystem/AuraBuff.cs
+++ Assets/Scripts/SkillSystem/AuraBuff.cs
@@ -1,20 +1,51 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
 
 public class AuraBuff : MonoBehaviour
 {
-    private List<Target> targets; 
+    private Range aura = null;
 
-	// Use this for initialization
-	void Start () 
+    public Skill skill;
+    public List<TargetType> types;
+    public List<Team.TeamIdentifier> IDs;
+
+    private float minValue = 1;
+    private float radius = 10;
+
+    private List<Target> targets;
+
+    void Awake()
     {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
+        types.Add(TargetType.Minion);
+        IDs.Add(GetComponent<Team>().ID);
+
+        aura.SetRelevantTargetTypes(types);
+        aura.SetRelevantTargetTeams(IDs);
+        aura.AddListener(RangeEvent.EnterRange, OnEnter);
+        aura.AddListener(RangeEvent.ExitRange, OnExit);
+    }
+
+    private void OnEnter(Target target)
     {
-	
-	}
+        targets.Add(target);
+    }
+
+    private void OnExit(Target target)
+    {
+        targets.Remove(target);
+    }
+
+    //public void RemoveEffects()
+    //{
+    //    targets = aura.GetTargetsByTypesAndTeam(types, IDs);
+    //    foreach (Target target in targets)
+    //        foreach (BuffBehaviour buff in target.gameObject.GetComponents<BuffBehaviour>())
+    //            if (buff.buffID == buffName)
+    //                buff.Remove();
+    //}
+
+    void Update()
+    {
+
+    }
 }
