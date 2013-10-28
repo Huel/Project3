e8aa22b14d0061710385cee801ef9b49eab2b022
Anton Licht <anton.licht@gmx.de>
2013-10-28 19:48:45 +0100

    Reimplement NetworkManager

---
+++ Assets/Scripts/NewGUI/LayoutSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class LayoutSettings : MonoBehaviour
+{
+
+    public Color32 ColorNormal;
+    public Color32 ColorFocus;
+    public Color32 ColorHover;
+    public Color32 ColorPressed;
+    public Color32 ColorDisabled;
+    public int TileSize;
+
+}
